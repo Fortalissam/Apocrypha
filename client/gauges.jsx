80908328918971977de040914c79909ae766e0cc
François Drouin-Morin <francois.drouinm@gmail.com>
2017-04-01 00:21:29 -0400

    Added recharts

--- client/gauges.jsx
+++ client/gauges.jsx
@@ -1,5 +1,6 @@
 var React = require("react");
 var Caroussel = require("./caroussel.jsx");
+import {BarChart, Bar, Legend, XAxis, YAxis, CartesianGrid, ResponsiveContainer} from "recharts";
 
 class GaugesContainer extends React.Component{
     constructor (props){
@@ -7,6 +8,10 @@ class GaugesContainer extends React.Component{
     }
 
     render(){
+        var data = [
+            {name: "ether", value: 5}
+        ];
+
         return (
             <div className="">
                 <h1>Some content</h1>
@@ -26,6 +31,14 @@ class GaugesContainer extends React.Component{
                             alert(json.message);
                         })
                 }}>Test</button>
+
+                <ResponsiveContainer height={400} width="100%">
+                    <BarChart data={data}>
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Bar dataKey="value" fill="#112233"/>
+                    </BarChart>
+                </ResponsiveContainer>
             </div>
         )
     }
